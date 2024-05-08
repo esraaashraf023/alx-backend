0dc09d502e752f489e083a126b8325ab48dba47b
esraaashraf023 <esraa@DESKTOP-GARLJAI.localdomain>
2024-05-08 08:55:30 +0300

    add task0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Basic dictionary"""
+from typing import Any
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Basic..
+    """
+
+    def put(self, key, item) -> None:
+        """ Store data on dictionary"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key) -> Any:
+        """ Get data from a dictionary"""
+        if key:
+            return self.cache_data.get(key, None)
